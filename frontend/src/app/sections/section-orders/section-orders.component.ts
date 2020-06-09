12d68e3248fcb065f7099d9adfaf327049b6bb13
mcmicko <mcmzrc@gmail.com>
2020-06-09 22:30:30 +0200

    fill sample in Order Section

--- frontend/src/app/sections/section-orders/section-orders.component.ts
+++ frontend/src/app/sections/section-orders/section-orders.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Order } from "../../shared/order";
 
 @Component({
-  selector: 'app-section-orders',
-  templateUrl: './section-orders.component.html',
-  styleUrls: ['./section-orders.component.css']
+  selector: "app-section-orders",
+  templateUrl: "./section-orders.component.html",
+  styleUrls: ["./section-orders.component.css"],
 })
 export class SectionOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "Milos Mandic",
+        state: "SRB",
+        email: "mandic@gmail.com",
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 3),
+    },
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "John Doe",
+        state: "UK",
+        email: "johnDoe@gmail.com",
+      },
+      total: 230,
+      placed: new Date(2012, 11, 21),
+      fulfilled: new Date(2018, 12, 3),
+    },
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "Liv Risa Ritter",
+        state: "GER",
+        email: "liv.ritter@gmail.com",
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 3),
+    },
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "Joaqim Murat",
+        state: "FRA",
+        email: "king.murat@gmail.com",
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 3),
+    },
+  ];
 
   ngOnInit(): void {
   }
-
 }
