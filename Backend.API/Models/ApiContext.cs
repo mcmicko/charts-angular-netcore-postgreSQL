f1dbed6515f02a7cf71fc774339580444df22966
mcmicko <mcmzrc@gmail.com>
2020-06-10 21:13:59 +0200

    create Models & DbContext

---
+++ Backend.API/Models/ApiContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.API.Models
+{
+  public class ApiContext : DbContext
+  {
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Server> Servers { get; set; }
+  }
+}
---
+++ Backend.API/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace Backend.API.Models
+{
+  public class Customer
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string State { get; set; }
+  }
+}
