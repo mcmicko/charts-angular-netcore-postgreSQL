84537355e3e072e48bfbdabcd6387926553f814f
mcmicko <mcmzrc@gmail.com>
2020-06-10 15:35:29 +0200

    imp change server status

--- frontend/src/app/server/server.component.ts
+++ frontend/src/app/server/server.component.ts
@@ -9,9 +9,30 @@ import { Server } from "../shared/server";
 export class ServerComponent implements OnInit {
   constructor() {}
 
+  color: string;
+  buttonText: string;
+
   @Input()
   serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = "#66bb6a";
+      this.buttonText = "Shut Down";
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = "#ff6b6b";
+      this.buttonText = "Start";
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ":", onlineStatus);
+    this.setServerStatus(!onlineStatus);
   }
 }
