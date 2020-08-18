09882584816d1c5ce1edb483bbba218bf3b4f381
mcmicko <mcmzrc@gmail.com>
2020-08-18 21:34:34 +0200

    service connect to backend

---
+++ frontend/src/app/service/server.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { Http, RequestOptions, Response, Headers } from "@angular/http";
+import { Observable } from "rxjs";
+import { ServerMessage } from "../shared/server-message";
+import { Server } from "../shared/server";
+
+import { map, catchError } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ServerService {
+  constructor(private _http: Http) {
+    this.headers = new Headers({
+      "Content-Type": "application/json",
+      Accept: "q=0.8;application/json;q=0.9",
+    });
+
+    this.options = new RequestOptions({ headers: this.headers });
+  }
+
+  options: RequestOptions;
+  headers: Headers;
+
+  getServers(): Observable<Server[]> {
+    return this._http
+      .get("http://localhost:5000/api/server")
+      .pipe(map((res) => res.json())).pipe(catchError(this.handleError));
+  }
+
+  handleError(error: any) {
+    const errMsg = error.message
+      ? error.message
+      : error.status
+      ? `${error.status} - ${error.statusText}`
+      : "Server error";
+
+    console.log(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  handleServerMessage(msg: ServerMessage): Observable<Response> {
+    const url = "http://localhost:5000/api/server/" + msg.id;
+    return this._http
+      .put(url, msg, this.options)
+      .pipe(map((res) => res.json()));
+  }
+}
